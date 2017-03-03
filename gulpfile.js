6dfcb1a9cf1b58e3559a55615a155c8ad7e08d3c
Carlos Junod <carlos@junod.cl>
2017-03-03 11:22:16 -0500

    adding files to my initial setup

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var reload      = browserSync.reload;
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+
+gulp.task('default', function() {
+
+});
+
+gulp.task('scss', function(){
+
+    var processors = [
+        autoprefixer({browsers: ['last 2 versions']}),
+    ];
+
+
+    return gulp.src('./scss/*.scss')
+    .pipe(sass())
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.reload({
+        stream:true
+    }))
+});
+
+gulp.task('browser-sync', function(){
+    browserSync.init({
+        server:{
+            baseDir: "./"
+        }
+    })
+})
+
+
+gulp.task('watch', ['browser-sync', 'scss'], function(){
+    // refresh.listen()
+    gulp.watch(['./scss/*.scss'], ['scss'])
+    gulp.watch("*.html").on("change", reload);
+})
