170c99e8db1309d72a93c21f29706305ea22856b
Carlos Junod <carlos@junod.cl>
2017-03-04 12:42:33 -0500

    Creating JS classes and printing it out on the HTML

--- js/main.js
+++ js/main.js
@@ -1,28 +1,81 @@
-var myRecipes = (function() {
-        var json = null;
-        $.ajax({
-            'async': false,
-            'global': false,
-            'url': "/js/myRecipes.json",
-            'dataType': "json",
-            'success': function (data) {
-                json = data;
-            }
-        });
-        return json;
-    })();
+$(document).ready(function(){
+    $('#modal').hide();
+    $('.layer-modal').hide();
+})
+
+var form = $('#js-add-recipe');
+var formButton = $('#js-add-recipe').find('button');
+
+// creating recipe object
+class Recipe {
+    constructor(title, preparation, rating) {
+        this.title = title;
+        this.preparation = preparation;
+        this.rating = rating;
+    }
+}
 
 
+
+$(formButton).on('click', function(e){
+    e.preventDefault()
+    let nameRecipe = $('#recipe-name').val();
+    let descRecipe = $('#recipe-desc').val();
+    let starsRecipe= $('#recipe-stars').val();
+
+    let myRecipe = new Recipe(nameRecipe, descRecipe, starsRecipe)
+
+    let container = $('<article class="recipe"></div>')
+
+    $(container).append('<h2>' + myRecipe.title + '</h2><p>'+ myRecipe.preparation +'</p>')
+
+    $('#recipes').append(container)
+    closeModal()
+
+})
+
+
+
+
+// var myRecipes = (function() {
+//         var json = null;
+//         $.ajax({
+//             'async': false,
+//             'global': false,
+//             'url': "/js/myRecipes.json",
+//             'dataType': "json",
+//             'success': function (data) {
+//                 json = data;
+//             }
+//         });
+//         return json;
+//     })();
+
+//opening modal
 $('#add').on('click', function(){
+    openModal()
+})
+
+// closing modal
+$('#close-button').on('click', function(){
+    closeModal()
+})
+
+function closeModal(){
+    $('#modal').fadeOut();
+    $('.layer-modal').delay(100).fadeOut(function(){
+        $(form).find('input').each(function(index, field){
+            $(field).val('');
+        })
+    });
+
+}
+
+function openModal(){
     $('#modal').delay(100).fadeIn();
     $('.layer-modal').fadeIn();
-})
-
-$('#close-button').on('click', function(){
-    $(this).closest('#modal').fadeOut();
-    $('.layer-modal').delay(100).fadeOut();
-})
+}
 
 var stars = $('.stars');
 
-console.log(myRecipes);
+//console.log(myRecipes);
